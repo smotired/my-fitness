77251a302b23385480ced9a196b1f1834ee07ed1
Sam Hill <soshjam@soshjam.com>
2026-09-02 09:38:47 -0600

    Run in IOS mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { setupIonicReact  } from '@ionic/react';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+setupIonicReact ({
+  mode: 'ios'
+});
 root.render(
   <React.StrictMode>
     <App />
