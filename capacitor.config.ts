086932b84436be943772611f31beb23d0273b540
Sam Hill <soshjam@soshjam.com>
2026-09-03 11:01:24 -0600

    Fix bundle ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'dev.whatinthesamhill.fitness',
+  appId: 'dev.whatinthesamhill.myfitness',
   appName: 'My Fitness',
   webDir: 'dist'
 };
